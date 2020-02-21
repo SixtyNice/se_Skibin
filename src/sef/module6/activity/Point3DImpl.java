6d2e9d82155b08587251d9439796f42df3b3ee48
Dima Skibin <sixtynice77@gmail.com>
2020-02-21 14:59:46 +0300

    Fix error use super methods and constructors

--- src/sef/module6/activity/Point3DImpl.java
+++ src/sef/module6/activity/Point3DImpl.java
@@ -16,6 +16,7 @@ public class Point3DImpl extends Point2DImpl implements Point3D {
      * Creates a Point3D object with the default coordinate of 0,0,0
      */
     public Point3DImpl() {
+        super();
         z = 0;
     }
 
@@ -27,8 +28,8 @@ public class Point3DImpl extends Point2DImpl implements Point3D {
      * @param z coordinate along the z axis
      */
     public Point3DImpl(double x, double y, double z) {
-        this.x = x;
-        this.y = y;
+
+        super(x, y);
         this.z = z;
 
     }
@@ -58,8 +59,7 @@ public class Point3DImpl extends Point2DImpl implements Point3D {
      * @see sef.module5.activity.Point3D#move(double, double, double)
      */
     public void move(double x2, double y2, double z2) {
-        this.x = x2;
-        this.y = y2;
+        super.move(x2, y2);
         this.z = z2;
 
     }
@@ -69,8 +69,7 @@ public class Point3DImpl extends Point2DImpl implements Point3D {
      * @see sef.module5.activity.Point3D#translate(double, double, double)
      */
     public void translate(double x2, double y2, double z2) {
-        x += x2;
-        y += y2;
+        super.translate(x2, y2);
         z += z2;
     }
 
@@ -81,7 +80,7 @@ public class Point3DImpl extends Point2DImpl implements Point3D {
     @Override
     public boolean equals(double x2, double y2, double z2) {
 
-        return this.x == x2 && this.y == y2 && this.z == z2;
+        return super.equals(x2, y2) && this.z == z2;
     }
 
 
@@ -93,10 +92,8 @@ public class Point3DImpl extends Point2DImpl implements Point3D {
      */
     public boolean equals(Object p) {
 
-        if (this == p) return true;
-        if (p == null || getClass() != p.getClass()) return false;
         Point3DImpl point = (Point3DImpl) p;
-        return this.x == point.getX() && this.y == point.getY() && this.z == point.getZ();
+        return super.equals(p) && this.z == point.getZ();
 
     }
 
@@ -105,8 +102,8 @@ public class Point3DImpl extends Point2DImpl implements Point3D {
      * @see sef.module5.activity.Point3D#getDistance(sef.module5.activity.Point3D)
      */
     public double getDistance(Point3D p) {
+        return super.getDistance(p) + Math.sqrt(Math.pow(p.getZ() - z, 2));
 
-        return Math.sqrt(Math.pow(p.getX() - x, 2) + Math.pow(p.getY() - y, 2) + Math.pow(p.getZ() - z, 2));
     }
 
 
@@ -115,8 +112,8 @@ public class Point3DImpl extends Point2DImpl implements Point3D {
      */
     public double getDistance(double x2, double y2, double z2) {
 
+        return super.getDistance(x2, y2) + Math.sqrt(Math.pow(z2 - z, 2));
 
-        return Math.sqrt(Math.pow(x2 - x, 2) + Math.pow(y2 - y, 2) + Math.pow(y2 - y, 2));
 
     }
 
